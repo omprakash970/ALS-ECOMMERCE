72e0dfbaa6588209e339b082e9bbe3af7f5971f7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-19 18:51:34 +0530

    done

--- practice/src/03_special.ts
+++ practice/src/03_special.ts
@@ -8,6 +8,9 @@ log('Hello World')
 
 //Do Not Use Any as much as possible - it defeats the purpose of using typescript in the first place
 
+const valueAny:any = JSON.parse('{"x":1}');
+valueAny.notThere.toFixed(2);  //this compiles fine but will throw an error at runtime
+
 
 
 function fail(message: string):never{
