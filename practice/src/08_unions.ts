b6ad6846340bdd56e63ef1b36993ba0abf146efc
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-19 21:07:00 +0530

    done

---
+++ practice/src/08_unions.ts
@@ -0,0 +1,35 @@
+function printId(id:number|string){
+  if(typeof id==="string"){
+   id.toLocaleLowerCase();
+  }else{
+   id.toFixed(2);
+  }
+}
+//object union 
+type Admin = {
+  role: 'Admin';
+  permissions:string[]; 
+}
+type Customer={
+  role:'Customer';
+  loyalityPoints:number;
+}
+function DescribeUser(user:Admin|Customer){
+  if(user.role==='Admin'){
+    console.log(user.permissions.join(', '));
+  }
+  else{
+    console.log(user.loyalityPoints);
+  }
+}
+function describeUserwithInOperator(user:Admin|Customer){
+  if('persmissions' in user){
+    console.log(user.role, 'Admin User'); 
+  }
+  if('loyalityPoints' in user){
+    console.log(user.role, 'Customer User'); 
+  }
+
+}
+const arrofUnion:(string|number)[]=["a", 1, "b", "2"]; 
+const unionOfArrays:string[]|number[]=["a", "b", "c"]; 
