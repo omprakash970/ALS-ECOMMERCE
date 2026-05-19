4533bee6f140f8aaa9a7a72f2b45edd0e5aad381
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-19 18:07:41 +0530

    done

--- practice/01_interface.ts
+++ practice/01_interface.ts
@@ -1,2 +1,10 @@
 //goal - understand when to let typescript infer types 
 //ts - js very well 
+//when ts will write the types / u r going to write 
+
+let count=0; //ts sees it as number
+const site = 'imoppie.tech' //ts sees it as string
+const scores = [100, 98, 95] //ts sees it as number[]
+ 
+
+//Over annotation isn't bad 
