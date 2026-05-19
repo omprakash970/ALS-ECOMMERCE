a1f5fd1a099e3671a5ea6ec2fba5370677ea35d7
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-19 19:00:32 +0530

    done

--- practice/src/04_objects.ts
+++ practice/src/04_objects.ts
@@ -12,4 +12,18 @@ const user1:User={
   createdAt:new Date(), 
   email:'2400032058@kluniversity.in'
 }
+const user2:User={
+  id:2400033019,
+  name:'Nancy', 
+  createdAt:new Date(),
+  email:'2400033019@kluniversity.in'
+}
+
+type Count = {
+  [k:string]: number, 
+}
+type Count1 = Record<"likes"|"views"|"shares",number>
+const c1 :Count={whatever:1}
+const c2:Count1={likes:100, views:1000, shares:500} //error because random is not in the keys of Count1
+
 
