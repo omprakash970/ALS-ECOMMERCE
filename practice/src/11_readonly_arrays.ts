1b70e47b0e60acf1f0383f919670bd398b8c4545
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-20 11:38:45 +0530

    done

---
+++ practice/src/11_readonly_arrays.ts
@@ -0,0 +1,13 @@
+const ys =[1,2,3] as const; 
+const nums:readonly number[]=[4, 5, 6];
+
+
+let s:number; 
+
+function sum(arr:readonly number[]):number{
+  for(const n of nums){
+    s+=n; 
+  }
+  return s;
+}
+
