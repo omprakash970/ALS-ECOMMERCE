202cda83b5f077c313142b2a67136b371b7ba6aa
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-20 11:35:43 +0530

    done

---
+++ practice/src/10_arrays_basics.ts
@@ -0,0 +1,6 @@
+const all =[1, 2, 3]; 
+const allStrings:string[] = ['a', 'b', 'c'];
+
+const a11 : number[]=[1, 2, 3];
+const a22: Array<number> =[4, 5, 6]; //Arrat<T>
+const scores =[100, 98, 95]; //ts infers it as number[
