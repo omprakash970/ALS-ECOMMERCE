72eceda09f140b828c6ea01a042cdb2f2101543b
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-20 13:09:05 +0530

    done

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,25 @@
 import 'dotenv/config'; 
 import express from 'express';
 import { connectDB } from './db';
+import cors from 'cors';
+import morgan from 'morgan';
+
 
 async function mainEntryFunction(){
   await connectDB(); 
   const app = express(); 
+
+  const corsOrigins =(process.env.CORS_ORIGIN ||"http://localhost:3000")
+  .split(',').map(origin=>origin.trim()).filter(Boolean); 
   
+
+  app.use(
+    cors({
+      origin:corsOrigins, 
+      credentials:true
+    })
+  )
+  app.use(express.json());
+  app.use(morgan('dev'));
+ 
 }
